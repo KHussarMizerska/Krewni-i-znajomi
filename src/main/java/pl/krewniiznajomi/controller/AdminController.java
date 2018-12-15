72273869222873b4dc663451b61f08cfc6287c5b
Katarzyna Hussar-Mizerska <khussar@gmail.com>
2018-12-15 18:14:45 +0100

    15.12.2018

--- src/main/java/pl/krewniiznajomi/controller/AdminController.java
+++ src/main/java/pl/krewniiznajomi/controller/AdminController.java
@@ -1,8 +1,15 @@
 package pl.krewniiznajomi.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminController {
 
@@ -10,8 +17,17 @@ public class AdminController {
     private Button btnShowAll;
 
     @FXML
-    void showAll(MouseEvent event) {
+    void showAll(MouseEvent event) throws IOException {
 
+        Stage adminStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/allDView.fxml"));
+        adminStage.setTitle("Lista wszystkich dorosłych w bazie");
+        adminStage.setScene(new Scene(root));
+        adminStage.show();
+
+        ((Node) event.getSource()).getScene()
+                .getWindow()
+                .hide();
     }
 
-}
+  }
