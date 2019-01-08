9b254edec368002db848b694a4423e96c082c3f8
Katarzyna Hussar-Mizerska <khussar@gmail.com>
2019-01-08 22:52:53 +0100

    statystyki imion dzieci

--- src/main/java/pl/krewniiznajomi/service/DorosliService.java
+++ src/main/java/pl/krewniiznajomi/service/DorosliService.java
@@ -3,6 +3,7 @@ package pl.krewniiznajomi.service;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import pl.krewniiznajomi.model.Dorosli;
+import pl.krewniiznajomi.model.dto.StatDorosliDTO;
 import pl.krewniiznajomi.utils.HibernateUtils;
 
 import java.util.List;
@@ -11,26 +12,38 @@ public class DorosliService {
 
     public List<Dorosli> pokazDorosli() {
 
-        Session session = HibernateUtils.getSessionFactory().openSession();
+        Session session = HibernateUtils.getSessionFactory()
+                .openSession();
         Transaction transaction = session.beginTransaction();
 
-        List<Dorosli> pokazDorosli = session.createQuery("SELECT d FROM Dorosli d").list();
+        List<Dorosli> pokazDorosli = session.createQuery("SELECT d FROM Dorosli d")
+                .list();
 
         transaction.commit();
         session.close();
         return pokazDorosli;
     }
 
-    public List<Dorosli> imionaDorosli() {
-
+    public List<StatDorosliDTO> imionaDorosli() {
         Session session = HibernateUtils.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
 
-        List<Dorosli> imionaDorosli = session.createSQLQuery("SELECT imie, count(imie) as ile FROM dorosli GROUP BY imie ORDER BY ile DESC, imie").addEntity(Dorosli.class).list();
-
+        List<StatDorosliDTO> imionaDorosli = session.createQuery("SELECT new pl.krewniiznajomi.model.dto.StatDorosliDTO(imie, count(imie) as ile) FROM Dorosli GROUP BY imie " +
+                "ORDER BY ile DESC, imie").list();
         transaction.commit();
         session.close();
         return imionaDorosli;
+    }
 
+
+    public List<StatDorosliDTO> lataUrDorosli() {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<StatDorosliDTO> lataUrDorosli = session.createQuery("SELECT new pl.krewniiznajomi.model.dto.StatDorosliDTO(year(data_ur) as rok, count(rok) as ile) FROM Dorosli GROUP BY rok " +
+                "ORDER BY ile DESC, rok").list();
+        transaction.commit();
+        session.close();
+        return lataUrDorosli;
     }
 }
