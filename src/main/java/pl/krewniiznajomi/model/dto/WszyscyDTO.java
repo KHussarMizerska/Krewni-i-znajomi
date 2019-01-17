607e036fdb1a88109ce33056a2b56513a5b36d01
Katarzyna Hussar-Mizerska <khussar@gmail.com>
2019-01-17 19:38:05 +0100

    WszyscyDTO

---
+++ src/main/java/pl/krewniiznajomi/model/dto/WszyscyDTO.java
@@ -0,0 +1,86 @@
+package pl.krewniiznajomi.model.dto;
+
+import java.util.Date;
+
+public class WszyscyDTO {
+
+    private String imie;
+
+    private String nazwisko;
+
+    private Date dataUr;
+
+    private Long wiekWdniach;
+
+    public WszyscyDTO() {
+    }
+
+    public WszyscyDTO(String imie, String nazwisko, Date dataUr) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.dataUr = dataUr;
+    }
+
+    public WszyscyDTO(String imie, String nazwisko, Date dataUr, Long wiekWdniach) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.dataUr = dataUr;
+        this.wiekWdniach = wiekWdniach;
+    }
+
+    public WszyscyDTO(String imie, String nazwisko, Date dataUr, Integer wiekWdniach) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.dataUr = dataUr;
+        this.wiekWdniach = wiekWdniach.longValue();
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public Date getDataUr() {
+        return dataUr;
+    }
+
+    public void setDataUr(Date dataUr) {
+        this.dataUr = dataUr;
+    }
+
+    public Long getWiekWdniach() {
+        return wiekWdniach;
+    }
+
+    public void setWiekWdniach(Long wiekWdniach) {
+        this.wiekWdniach = wiekWdniach;
+    }
+
+    @Override
+    public String toString() {
+        return "Wszyscy{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", dataUr=" + dataUr +
+                ", wiekWdniach=" + wiekWdniach +
+                '}';
+    }
+}
+
+
+
+
+
+
+
